5f634966d33ef8210ec028be15519c0d1fca7c6c
A_Ariannejad <ariannejad14@gmail.com>
2023-07-08 16:14:39 +0430

    Set limit Activity logic is created

---
+++ app/src/main/java/com/firstapp/datatracker/SetLimitActivity.kt
@@ -0,0 +1,31 @@
+package com.firstapp.datatracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.firstapp.datatracker.databinding.ActivitySetLimitBinding
+
+
+class SetLimitActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySetLimitBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_set_limit)
+
+        binding.currentLimit.text = "مقدار فعلی محدودیت: " + (ProfilePreferences.getLimit()/1024/1024).toString() + "مگابایت"
+
+        binding.submitButton.setOnClickListener {
+            if (!binding.editText.text.toString().isNullOrEmpty()){
+                ProfilePreferences.setLimit(binding.editText.text.toString().toInt() * 1024 * 1024)
+                ProfilePreferences.setNotification(false)
+                finish()
+            }
+            else{
+                binding.editText.error = "این فیلد نباید خالی باشد"
+            }
+        }
+
+    }
+}
